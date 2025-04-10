906cb9966e701a421f2f15d33acd6e7e4e71e48c
Harchimemedes <474890266@qq.com>
2025-04-10 17:53:49 +0800

    修改LUT为阳码，增加正常模式画字符串，增加快速画方框

--- CH585apps/EPD_Bare/EPD/miniGUI.c
+++ CH585apps/EPD_Bare/EPD/miniGUI.c
@@ -46,6 +46,13 @@ static inline void setPixel(uint16_t x, uint16_t y, uint8_t color)
 
 }
 
+//放大式地画像素，比如2倍放大就会在对应位置画四个像素。注意xy值也会被相应的放大。
+__HIGH_CODE
+static inline void scaledSetPixel(uint16_t x, uint16_t y, uint8_t color, uint8_t scale)
+{
+	;//暂未实现。
+}
+
 //画线。只能画横着或者竖着的线。斜着的线没有必要。
 void drawLine(uint16_t xStart, uint16_t yStart, uint16_t xEnd, uint16_t yEnd, uint8_t color)
 {
@@ -79,6 +86,7 @@ void drawRect(uint16_t xStart, uint16_t yStart, uint16_t xEnd, uint16_t yEnd, ui
 	drawLine(xStart, yEnd, xStart, yStart, color);
 }
 
+
 //画实心方块
 void fillRect(uint16_t xStart, uint16_t yStart, uint16_t xEnd, uint16_t yEnd, uint8_t color)
 {
@@ -99,8 +107,8 @@ void fastFill(uint16_t x, uint16_t y, uint16_t xblock, uint16_t yblock, uint8_t 
 	uint8_t tmp;
 	//这是边缘的像素块，需要相应的移位到正确的地方
 	//同时需要利用-1移位是补1不是补0
-	int8_t mask1 = color ? (-1) << (7-(x+xblock)&7) : ~(0xFF<<(7-(x+xblock)&7));
-	int8_t mask2 = color ? ~((-1) << (7-(x-1)&7))       : (0xFF)<<(7-(x-1)&7);
+	int8_t mask1 = color ? ~(0xFF >> ((x+xblock+1)&7)) : (0xFF >> ((x+xblock+1)&7));
+	int8_t mask2 = color ?  (0xFF>>(x&7)) : ~(0xFF>>(x&7));
 
 	//覆盖对应的位置，先覆盖头，再覆盖中间，最后覆盖尾。
     for( uint16_t i = y; i <= (y+yblock); i++ )
@@ -122,6 +130,52 @@ void fastFill(uint16_t x, uint16_t y, uint16_t xblock, uint16_t yblock, uint8_t 
 
 }
 
+//快速画方框，注意会清空中间的部分，且不检查参数顺序是否合理,输入的End必须大于
+//start。
+void fastRect(uint16_t xStart, uint16_t yStart, uint16_t xEnd, uint16_t yEnd, uint8_t color)
+{	
+	uint16_t xblock = xEnd-xStart;
+	uint16_t yblock = yEnd-yStart;
+	uint16_t index;
+	uint8_t tmp;
+	//这是边缘的像素块，需要相应的移位到正确的地方
+	//同时需要利用-1移位是补1不是补0
+	int8_t mask1 = color ? 0x80 >> ((xEnd)&7) : ~(0x80>>((xEnd)&7));
+	int8_t mask2 = color ? 0x80 >> ((xStart)&7) : ~(0x80)>>((xStart)&7);
+
+	//覆盖对应的位置，先覆盖头尾。
+    for( uint16_t i = yStart; i <= (yEnd); i++ )
+    {	
+		index = (xStart>>3) + (i<<4);
+		tmp = image[index];
+		image[index] = color ? ( tmp | mask2 ) : ( tmp & mask2 );
+
+		if( i==yStart || i==yEnd )
+		{
+			tmp = image[(xStart>>3)+(i<<4)];
+			image[(xStart>>3)+(i<<4)] = color ? 
+			                            (tmp | (0xFF>>(xStart&7))) :
+			                            (tmp & ~(0xFF>>(xStart&7)));
+			                            
+			tmp = image[(xEnd>>3)+(i<<4)];
+			image[(xEnd>>3)+(i<<4)] = color ?
+			                          (tmp | ~(0xFF>>(xEnd&7))) :
+			                          (tmp & (0xFF>>(xEnd&7))); 
+	    	for (uint16_t j = xStart+8; j < xEnd; j += 8 )
+	    	{
+	    		index = (j>>3) + (i<<4);
+	    		image[index] = color;
+	    		
+			}
+			
+		}				
+		index = (xEnd>>3) + (i<<4);
+		tmp = image[index];
+		image[index] = color ? ( tmp | mask1 ) : ( tmp & mask1 );
+    }	
+	
+}
+
 
 //快速显示图像，图像的的宽度必须是128，两个参数指定了图像
 //在屏幕上的起点和终点。
@@ -133,7 +187,7 @@ void FastImg(uint16_t xStart, uint16_t xEnd, const char *imgDat)
 	length);
 }
 
-//x轴只支持字节对齐，也就是只有16行,
+//快速画单个字，x轴只支持字节对齐，也就是只有16行,
 void fastDrawChar(uint16_t xStart, uint16_t yStart, char chara, const uint8_t *font)
 {	
 	if (FONT_GETHEIGHT(font) == 0x08)
@@ -142,11 +196,12 @@ void fastDrawChar(uint16_t xStart, uint16_t yStart, char chara, const uint8_t *f
 	    for (uint8_t i=0; i<width; i++)
 	    {
 	    	uint16_t index = (xStart>>3)+((yStart+i)<<4);
-	    	image[index] = ~font[width*(chara-' '+1)+i];
+	    	image[index] = font[width*(chara-' '+1)+i];
 	    }	
 	}
 }
 
+//快速画字符串。
 void fastDrawString(uint16_t xStart, uint16_t yStart,char *stringToPrint, const uint8_t *font)
 {
 	if (FONT_GETHEIGHT(font) == 0x08)
@@ -159,4 +214,35 @@ void fastDrawString(uint16_t xStart, uint16_t yStart,char *stringToPrint, const 
 			fastDrawChar(xStart,yStart-i*7,*stringToPrint,font8);	
 		}	
 	}
+}
+
+//正常画单个字符，可支持12、18等非8字节对齐的字符大小和任意位置，可指定颜色
+void drawChar(uint16_t xStart, uint16_t yStart, char charToPrint, const char *font, uint8_t color)
+{
+	uint8_t height = FONT_GETHEIGHT(font);
+	uint8_t width = FONT_GETWIDTH(font);
+	for (int i = 0; i<width; i++)
+	{
+		for(int j = 0; j<height; j++)
+		{
+			if(font[width*(charToPrint-' '+1)+i] & 0x80>>j)
+				{setPixel(xStart+j,yStart+i,color);}
+			else
+				{setPixel(xStart+j,yStart+i,~color);}
+		}
+	}
+
+}
+
+//正常画字符串
+void drawStr(uint16_t xStart, uint16_t yStart,char *stringToPrint, const char *font, uint8_t color)
+{
+	int i = 0;
+	int width = FONT_GETWIDTH(font);
+	for( ; *stringToPrint; stringToPrint++)
+	{
+	    i++;
+		drawChar(xStart,yStart-i*width,*stringToPrint,font8,color);	
+	}	
+	
 }
--- CH585apps/EPD_Bare/main.c
+++ CH585apps/EPD_Bare/main.c
@@ -10,7 +10,7 @@
 __attribute__((aligned(4))) uint8_t imageCache[4736] = {0};//显存，为了提高memcpy的速度需要四字节对齐。
 
 //和memset
-static inline void mymemset(void *dest, int c, size_t n) { unsigned char *s = dest; for (; n; n--, s++) *s = c; }
+static inline void mymemset(void *dest, int c, size_t n) { unsigned char *s = dest; for (; n; n--, s+=4) *s = c; }
 
 static void GPIOInit(void)
 {
@@ -27,40 +27,39 @@ void main(void)
 	GPIOInit();
 
 	GPIOB_SetBits(GPIO_Pin_6);
-	mymemset(imageCache,0xFF,4736);
+	//由于LUT反向，不再需要初始化，0x00对应白色
+	//memset(imageCache,0xFF,4736);
 	
 	paint_SetImageCache(imageCache);
 
-	FastImg(147,295,gImage_full);
-	
-	drawLine(0,147,127,147,BLACK);
-	drawRect(0,0,127,295,BLACK);
+	//显示图片,100us左右
+	FastImg(147,295,gImage_full+16*10);
 
+	//需要setpixel的画图，3-4毫秒
+//	drawLine(0,147,127,147,BLACK);
+	drawRect(0,0,127,295,BLACK);
 	drawRect(10,10,50,50,BLACK);
 
+	//快速画图与快速显示字符串，900us
 	fastFill(10,52,40,40,BLACK);
 	fastFill(23,64,2,2,WHITE);
-
+//	fastRect(0,0,127,295,BLACK);
+//	fastRect(10,10,50,50,BLACK);
 	fastDrawChar(80,10,'7',font8);
 	fastDrawChar(80,17,'=',font8);
-	fastDrawChar(80,24,'X',font8);
-	fastDrawChar(80,31,'s',font8);
-
+	fastDrawChar(80,24,'s',font8);
+	fastDrawChar(80,31,'x',font8);
 	fastDrawString(88,81,"12345abc",font8);
-/*
-	uint8_t *tstring = (char *)"yuanshen";
-	int i = 0;
-	for( ; *tstring; tstring++)
-	{
-	    i++;
-		fastDrawChar(96,100-i*7,*tstring,font8);	
-	}
-*/	
+
+	//需要setpixel的显示字符串，几个毫秒
+//	drawChar(77,50,'y',font8,WHITE);
+//	drawChar(77,50,'y',font8,WHITE);
+//	drawChar(77,50,'y',font8,WHITE);
+//	drawChar(77,50,'y',font8,WHITE);
+//	drawStr(60,81,"12345abc",font8,WHITE);
 	
 	GPIOB_ResetBits(GPIO_Pin_6);
-	EPD_Init();
-	
-	//tickDelayMs(15);
+	EPD_Init();	
 	EPD_SendDisplay(imageCache);
 	EPD_Sleep();
 	//tickDelayMs(3000);
