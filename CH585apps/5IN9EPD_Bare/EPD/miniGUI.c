a03422438d22f8d6dc08e42e4aac2b7d79b8e31f
Harchimemedes <474890266@qq.com>
2025-04-26 15:21:05 +0800

    去掉一个不必要的取模

--- CH585apps/5IN9EPD_Bare/EPD/miniGUI.c
+++ CH585apps/5IN9EPD_Bare/EPD/miniGUI.c
@@ -24,8 +24,8 @@ void paint_SetImageCache(uint8_t *imagePtr)
 __HIGH_CODE
 static inline void setPixel(uint16_t x, uint16_t y, uint8_t color)
 {
-	//单周期乘法以及硬件除法给我的底气
-    uint16_t index = ((x>>3)*272) + (y%272); 
+	//单周期乘法给我的底气
+    uint16_t index = ((x>>3)*272) + y; 
     //像素所对应的字节的位置
     if( color )
     {
