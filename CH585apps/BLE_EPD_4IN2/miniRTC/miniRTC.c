08563cb50e4af63b136c198de162be653a387f2c
Harchimemedes <474890266@qq.com>
2025-06-29 03:11:38 +0800

    修复迷你RTC在零点整会输出24：00而不是00：00的问题。

--- CH585apps/BLE_EPD_4IN2/miniRTC/miniRTC.c
+++ CH585apps/BLE_EPD_4IN2/miniRTC/miniRTC.c
@@ -100,8 +100,8 @@ void miniRTC_GetTime(
 	//应用offset
     t+=secondsOffset;
     day+=dayOffset;
-    day = t > 86400 ? day + 1 : day; //如果t大于86400，说明已经过了一天
-    t = t > 86400 ? t-86400 : t; //确保t在0-86399之间
+    day = t > 86399 ? day + 1 : day; //如果t大于86400，说明已经过了一天
+    t = t > 86399 ? t-86400 : t; //确保t在0-86399之间
     
 
     *year = BEGYEAR;
